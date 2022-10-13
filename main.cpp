c91e696567389f9a407fe64225ff6a5aabd1ef6b
henry0523ho <henry0523ho.3@gmail.com>
2022-10-14 02:19:41 +0800

    init main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "1\n";
+    cout << "2\n";
+    cout << "3\n";
+    cout << "4\n";
+    cout << "5\n";
+    return 0;
+}
